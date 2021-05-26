283a7179b67cf384462edb023515170f8da46a72
Prakhar Singh <prakhars76@gmail.com>
2021-05-26 21:07:19 +0530

    Added Some Modules

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,15 +1,18 @@
 import { createStackNavigator } from 'react-navigation-stack';
+//import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 import { Platform } from 'react-native';
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import CartScreen from '../screens/shop/CartScreen';
 import Colors from '../constants/Colors';
 
 const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,
-    ProductDetail: ProductDetailScreen
+    ProductDetail: ProductDetailScreen,
+    Cart: CartScreen
   },
   {
     defaultNavigationOptions: {
