1a49c08d65947a2cf27c159bd0326574532276b2
Prakhar Singh <prakhars76@gmail.com>
2021-05-31 21:44:13 +0530

    fixed error

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import AppLoading from 'expo-app-loading';
+import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 
 import productsReducer from './store/reducers/products';
@@ -34,7 +34,6 @@ export default function App() {
         onFinish={() => {
           setFontLoaded(true);
         }}
-        onError={(err) => console.log(err)}
       />
     );
   }
